5fc425d5c2dbd8c5921cd425389ec09600d59413
bila200306 <nabilainnas3@gmail.com>
2025-04-27 15:27:40 +0700

    update tambah terminal

--- tambah_terminal&gate.php
+++ tambah_terminal&gate.php
@@ -10,8 +10,12 @@ ob_start();
 <div class="container mt-4">
   <form action="" method="post">
     <div class="form-group">
-      <label for="airlineName">Name Terminal & gate</label>
-      <input type="text" class="form-control" id="airlineName" name="airlineName" placeholder="Enter Terminal & Gate" required>
+      <label for="airlineName">Name Terminal</label>
+      <input type="text" class="form-control" id="airlineName" name="airlineName" placeholder="Enter Terminal " required>
+    </div>
+    <div class="form-group">
+      <label for="airlineName">Name gate</label>
+      <input type="text" class="form-control" id="airlineName" name="airlineName" placeholder="Enter Gate" required>
     </div>
     <button type="submit" class="btn btn-primary"> <i class="fas fa-save"></i> Save</button>
   </form>

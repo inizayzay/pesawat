afe726b435ab7d77554804a444b27e1adcf600ff
zzay <zacky29gaming@gmail.com>
2025-05-10 14:30:07 +0700

    Fix terminal gate function

--- functions/terminal_gate.php
+++ functions/terminal_gate.php
@@ -4,6 +4,44 @@ namespace TerminalGate;
 
 require_once dirname(__FILE__) . "/../connections/database.php";
 
+function store()
+{
+    global $mysql;
+
+    $terminal = $_POST['Terminal'];
+    $gate = $_POST['Gate'];
+
+    $sql = "INSERT INTO terminal_gate (Terminal,Gate) VALUES (?,?)";
+    $stmt = $mysql->prepare($sql);
+    $stmt->bind_param("ss", $terminal, $gate);
+
+    if ($stmt->execute()) {
+        echo "Airline berhasil ditambahkan!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+}
+
+function update()
+{
+    global $mysql;
+
+    $airlineName = $_POST['airlineName'];
+
+    $sql = "UPDATE Airline 
+    SET AirlineName = ?
+    WHERE AirlineID = ?";
+
+    $stmt = $mysql->prepare($sql);
+    $stmt->bind_param("ssi", $airlineName, $airlineID);
+
+    if ($stmt->execute()) {
+        echo "Airline berhasil diupdate!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+}
+
 function get()
 {
     global $mysql;
@@ -14,10 +52,10 @@ function get()
 }
 
 
-function delete($id) {
+function delete($id)
+{
     global $mysql;
     $stmt = $mysql->prepare("DELETE FROM terminal_gate WHERE ID = ?");
     $stmt->bind_param("i", $id);
     $stmt->execute();
 }
-

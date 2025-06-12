7fc09f00aa6d452cd1bb4a6008465d8d68776f7f
bila200306 <nabilainnas3@gmail.com>
2025-06-12 09:03:14 +0700

    djhasiaf

--- components/sidebar.php
+++ components/sidebar.php
@@ -3,7 +3,7 @@ $currentPath = basename($_SERVER['PHP_SELF']);
 
 $navItems = [
     ['title' => 'Dashboard', 'href' => 'index.php', 'icon' => 'fas fa-fw fa-tachometer-alt', 'Role' => 'passenger'],
-    ['title' => 'Airline', 'href' => 'maskapai.php', 'icon' => 'fas fa-fw fa-plane', 'Role' => 'admin'],
+    ['title' => 'Airline', 'href' => 'maskapai.php', 'icon' => 'fas fa-fw fa-plane', 'Role' => 'admin',],
     ['title' => 'Passenger', 'href' => 'penumpang.php', 'icon' => 'fas fa-fw fa-users', 'Role' => 'admin'],
     ['title' => 'Terminal & Gate', 'href' => 'terminal_gate.php', 'icon' => 'fas fa-fw fa-door-open', 'Role' => 'admin'],
     ['title' => 'Flight', 'href' => 'jadwal_penerbangan.php', 'icon' => 'fas fa-fw fa-calendar-alt', 'Role' => 'admin'],

bbefdd8b0447f84d8429c398b74f96bfb178ce89
Cyber Panel <nabilagacor88@gmail.com>
2025-06-18 11:29:03 +0000

    Database

--- connections/database.php
+++ connections/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysql = new mysqli('localhost:3306', 'root', '', 'tiket_pesawat');
+$mysql = new mysqli('localhost:3306', 'tiket_root', 'root', 'tiket_db');
 
 if ($mysql->connect_error) {
     die("Error when connecting to database");
